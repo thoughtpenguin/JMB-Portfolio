a70ac331de0f804204fcab68b8002ccf2f122ffe
James Berndt <jamesmberndt@gmail.com>
2026-04-03 15:16:48 -0500

    conftest file for Selenium tests

---
+++ SeleniumTests/conftest.py
@@ -0,0 +1,42 @@
+from selenium import webdriver
+from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
+import pytest
+import os
+
+def pytest_addoption(parser):
+    parser.addoption(
+        '--browser', action='store', default='Chrome', help='browser: chrome or firefox'
+    )
+
+
+@pytest.fixture
+def browser(request):
+    return request.config.getoption('--browser')
+
+@pytest.fixture
+def driver(browser):
+    driver = ''
+    if browser == 'firefox':
+        firefox_options = FirefoxOptions()
+        firefox_options.add_argument('--headless')
+        firefox_options.add_argument('--window-size=1920,1080')
+        service = FirefoxService(GeckoDriverManager().install())
+        driver = webdriver.Firefox(service=service, options=firefox_options)
+    else:
+        chrome_options = ChromeOptions()
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--disable-dev-shm-usage')
+        chrome_options.add_argument('--disable-gpu')
+        chrome_options.add_argument('--window-size=1920,1080')
+        service = ChromeService(ChromeDriverManager().install())
+        driver = webdriver.Chrome(service=service, options=chrome_options)
+    yield driver
+    driver.quit()
