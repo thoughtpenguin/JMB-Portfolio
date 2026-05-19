38546b834b620fa14a85c0ba71da92a834aa631c
James Berndt <jamesmberndt@gmail.com>
2026-05-19 12:42:51 -0500

    update assertion messages

--- SeleniumTests/test_add_remove_elements.py
+++ SeleniumTests/test_add_remove_elements.py
@@ -39,13 +39,13 @@ class TestAddRemoveElements:
 
     def test_no_added_elements_present(self, driver:webdriver.Remote, page_loaded):
         element_list = driver.find_elements(By.CLASS_NAME, ADDED_MANUALLY)
-        assert len(element_list) == 0
+        assert len(element_list) == 0, "No buttons should be present."
         return
     
     def test_add_one_element(self, driver:webdriver.Remote, add_element_button:WebElement):
         add_element_button.click()
         delete_buttons = driver.find_elements(By.CLASS_NAME, ADDED_MANUALLY)
-        assert len(delete_buttons) == 1
+        assert len(delete_buttons) == 1, "One button should be present."
         return
     
     def test_add_five_elements(self, driver:webdriver.Remote, add_element_button:WebElement):
@@ -55,7 +55,7 @@ class TestAddRemoveElements:
             add_element_button.click()
             i += 1
         delete_buttons = driver.find_elements(By.CLASS_NAME, ADDED_MANUALLY)
-        assert len(delete_buttons) == 5
+        assert len(delete_buttons) == 5, "Five buttons should be present."
         return
     
     def test_add_two_remove_one(self, driver:webdriver.Remote, add_element_button:WebElement):
@@ -67,14 +67,11 @@ class TestAddRemoveElements:
         clicked = delete_buttons[0]
         clicked.click()
         delete_buttons = driver.find_elements(By.CLASS_NAME, ADDED_MANUALLY)
-        #Confirm there is only one delete button
-        assert len(delete_buttons) == 1
-        #Confirm that the clicked button is no longer present
-        assert clicked not in delete_buttons
+        assert len(delete_buttons) == 1, "One button should be present."
+        assert clicked not in delete_buttons, "Deleted buttons should not be present."
         return
     
     def test_add_five_remove_five(self, driver:webdriver.Remote, add_element_button:WebElement):
-        # Add five buttons.
         i = 0
         while i < 5:
             add_element_button.click()
@@ -84,16 +81,15 @@ class TestAddRemoveElements:
         # Delete all of the buttons.
         for button in delete_buttons:
             button.click()
-        # Confirm there are no buttons.
         delete_buttons = driver.find_elements(By.CLASS_NAME, ADDED_MANUALLY)
-        assert len(delete_buttons) == 0
+        assert len(delete_buttons) == 0, "No buttons should be present."
         return
 
     def test_refresh_after_add(self, driver:webdriver.Remote, add_element_button:WebElement):
         add_element_button.click()
         driver.refresh()
         delete_buttons = driver.find_elements(By.CLASS_NAME, ADDED_MANUALLY)
-        assert len(delete_buttons) == 0
+        assert len(delete_buttons) == 0, "No buttons should be present."
         return
 
 #endregion
